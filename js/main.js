815ac5e582481d3efd233321d6c81a1c61ed6cb6
Godje TV <comepot.4anell@gmail.com>
2016-06-19 04:13:45 -0700

    Remake 2.0
    1.0

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var opts = {
+    count: 3,
+    size: 50,
+    sizeRandom: 10,
+    sparkLife: 0.1,
+    spawnOpacity: 1,
+
+    //must be RGBA. alpha stands for opacity, usually 1
+    color: "rgba(39, 173, 96, alpha)"
+  },
+
+  canvasBody = document.getElementById("canvas"),
+  canvas = canvasBody.getContext("2d"),
+  h = canvasBody.width = window.innerWidth,
+  w = canvasBody.height = window.innerHeight;
+//document.getElementsByClassName("card")[0].style["background-color"] = opts.color.replace("alp", "1");
+function step() {
+  var fillColor = opts.color.replace("alpha", opts.spawnOpacity);
+  canvas.fillStyle = fillColor;
+  for (var i = 0; i < Math.round(opts.count); i++) {
+    var random = Math.random() * opts.sizeRandom;
+    canvas.fillRect(-(opts.size/2) + Math.random() * h, -(opts.size/2) + Math.random() * w, opts.size + random, opts.size + random)
+  }
+  canvas.fillStyle = "rgba(255,255,255," + opts.sparkLife + ")"
+  canvas.fillRect(0, 0, h, w)
+}
+
+function anim() {
+  setTimeout(function() {
+    window.requestAnimationFrame(anim)
+  }, 1000 / 30)
+  step();
+}
+
+anim();
