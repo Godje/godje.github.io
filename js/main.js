bf24a893b122af2ac922584330e93aba6e8decea
Godje <comepot.4anell@gmail.com>
2016-11-22 00:04:50 -0800

    3.2.1
    Canvas resize fix

--- js/main.js
+++ js/main.js
@@ -11,18 +11,18 @@ var opts = {
 
   canvasBody = document.getElementById("canvas"),
   canvas = canvasBody.getContext("2d"),
-  h = canvasBody.width = window.innerWidth,
-  w = canvasBody.height = window.innerHeight;
+  w = canvasBody.width = window.innerWidth,
+  h = canvasBody.height = window.innerHeight;
 //document.getElementsByClassName("card")[0].style["background-color"] = opts.color.replace("alp", "1");
 function step() {
   var fillColor = opts.color.replace("alpha", opts.spawnOpacity);
   canvas.fillStyle = fillColor;
   for (var i = 0; i < Math.round(opts.count); i++) {
     var random = Math.random() * opts.sizeRandom;
-    canvas.fillRect(-(opts.size/2) + Math.random() * h, -(opts.size/2) + Math.random() * w, opts.size + random, opts.size + random)
+    canvas.fillRect(-(opts.size/2) + Math.random() * w, -(opts.size/2) + Math.random() * h, opts.size + random, opts.size + random)
   }
   canvas.fillStyle = "rgba(255,255,255," + opts.sparkLife + ")"
-  canvas.fillRect(0, 0, h, w)
+  canvas.fillRect(0, 0, w, h)
 }
 
 function anim() {
