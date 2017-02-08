25b27bb968b85bee70b50e85c76ecd1dcc331d7b
Godje <comepot.4anell@gmail.com>
2017-02-08 01:09:07 -0800

    Added more colors
    dark-red and green

--- otherRes/colors.js
+++ otherRes/colors.js
@@ -9,6 +9,16 @@ function Colors(){
       hex: "#e74c3c",
       rgb: "rgb(231, 76, 60)",
       rgba: "rgba(231, 76, 60,1.0)",
+    },
+    "dark-red": {
+      hex: "#c0392b",
+      rgb: "rgb(192, 57, 43)",
+      rgba: "rgba(192, 57, 43,1.0)",
+    },
+    green: {
+      hex: "#2ecc71",
+      rgb: "rgb(46, 204, 113)",
+      rgba: "rgba(46, 204, 113,1.0)"
     }
   };
   return arguments.length==1?Colors[arguments[0]].hex:Colors[arguments[0]][arguments[1]];
