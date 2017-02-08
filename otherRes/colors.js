fa4379859103c6aab03c382d16931811bd6f3639
Godje <comepot.4anell@gmail.com>
2017-02-07 18:02:05 -0800

    1

---
+++ otherRes/colors.js
@@ -0,0 +1,15 @@
+function Colors(){
+  var Colors = {
+    playdoh: {
+      hex: "#1abc9c",
+      rgb: "rgb(26, 188, 156)",
+      rgba: "rgba(26, 188, 156, 1.0)",
+    },
+    red: {
+      hex: "#e74c3c",
+      rgb: "rgb(231, 76, 60)",
+      rgba: "rgba(231, 76, 60,1.0)",
+    }
+  };
+  return arguments.length==1?Colors[col].hex:Colors[col][arguments[1]];
+}
